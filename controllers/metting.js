68817be7fc69dc4b17f90929d1d200b1f55a93c9
tingyugetc <B13040804@njupt.edu.cn>
2017-04-18 21:55:40 +0800

    create model

--- controllers/metting.js
+++ controllers/metting.js
@@ -3,10 +3,19 @@
  * 会议控制器
  */
 
+const Meeting = require('../models/metting').Meeting;
+
 exports.create = function (req, res, next) {
     var name = req.body.name;
     var start_time = req.body.start_time;
     var location = req.body.location;
+    var user = req.session.user;
+
+    Meeting.create({
+
+    }, function (err, meeting) {
+
+    });
 
     console.log(req.session);
     res.json({
