4bbbfce50325be8e15639b4d6361590402ab2551
latelater <903430068@qq.com>
2017-05-13 16:53:37 +0800

    new user info

--- public/js/camara.js
+++ public/js/camara.js
@@ -1,34 +1,35 @@
 // camara.js
 
-// var video = document.getElementById("video");
+var video = document.getElementById("video");
 // var context = canvas.getContext("2d");
-// var errocb = function () {
-//     console.log('sth wrong!');
-// }
+var errocb = function () {
+    console.log('sth wrong!');
+}
 
-// if (navigator.getUserMedia) { // 标准的API
-//     navigator.getUserMedia({ "video": true }, function (stream) {
-//         video.src = stream;
-//         video.play();
-//     }, errocb);
-// } else if (navigator.webkitGetUserMedia) { // WebKit 核心的API
-//     navigator.webkitGetUserMedia({ "video": true }, function (stream) {
-//         video.src = window.webkitURL.createObjectURL(stream);
-//         video.play();
-//     }, errocb);
-// }
+if (navigator.getUserMedia) { // 标准的API
+    navigator.getUserMedia({ "video": true }, function (stream) {
+        video.src = stream;
+        video.play();
+    }, errocb);
+} else if (navigator.webkitGetUserMedia) { // WebKit 核心的API
+    navigator.webkitGetUserMedia({ "video": true }, function (stream) {
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errocb);
+}
 
-// document.getElementById("picture").addEventListener("click", function () {
-//     context.drawImage(video, 0, 0, 640, 480);
-// });
+document.getElementById("picture").addEventListener("click", function () {
+    // context.drawImage(video, 0, 0, 640, 480);
+    
+});
 
-var p = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+// var p = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
 
-p.then(function(mediaStream) {
-	var video = document.querySelector('video');
-	link = window.URL.createObjectURL(mediaStream);
-	video.src = link;
-	video.onloadedmetadata = function(e) {
+// p.then(function(mediaStream) {
+	// var video = document.querySelector('video');
+	// link = window.URL.createObjectURL(mediaStream);
+	// video.src = link;
+	// video.onloadedmetadata = function(e) {
 	// Do something with the video here.
 		// var request = new XMLHttpRequest();
 		// request.open('GET',url);
@@ -38,12 +39,7 @@ p.then(function(mediaStream) {
 		// req.responseType = 'json';
 		// request.send(link);
 		
-	};
-});
+	// };
+// });
 
-p.catch(function(err) { console.log(err.name); }); // always check for errors at the end
-
-// var front = false;
-// document.getElementById('flip-button').onclick = function() { front = !front; };
-
-// var constraints = { video: { facingMode: (front? "user" : "environment") } };
+// p.catch(function(err) { console.log(err.name); }); // always check for errors at the end
